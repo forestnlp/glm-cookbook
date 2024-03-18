b75a0064f454be8d29d51b0ec7e4da114cfb45ba
forestnlp <19085498@qq.com>
2024-03-18 22:56:09 +0800

    homework

--- characterGLM/auto_conversation.py
+++ characterGLM/auto_conversation.py
@@ -3,16 +3,28 @@ from api import generate_role_NameandInfo,get_characterglm_response
 import json
 
 class CharacterAgent:
-    def __init__(self, messages, user_name, user_info, bot_name, bot_info):
-        self.messages = messages
-        self.user_name = user_name
-        self.user_info = user_info
-        self.bot_name = bot_name
-        self.bot_info = bot_info
+	def __init__(self, messages, user_name, user_info, bot_name, bot_info):
+		self.messages = messages
+		self.user_name = user_name
+		self.user_info = user_info
+		self.bot_name = bot_name
+		self.bot_info = bot_info
+		self.character_meta = {
+		"user_info": user_info,
+		"bot_info": bot_info,
+		"user_name": user_name,
+		"bot_name": bot_name
+		}
 
-    def chat(self, message):
-        self.messages.append(message)
-        return message
+	def chat(self, message):
+     
+		if(len(self.messages)>=5):
+			self.messages.pop()
+				
+		self.messages.append({"role": "user", "content": message})
+		response = ''.join(get_characterglm_response(self.messages, meta=self.character_meta))
+		self.messages.append({"role": "assistant", "content": response})
+		return response
 
 
 def generate_dialogue(character_meta_1, character_meta_2):
@@ -26,19 +38,45 @@ def generate_dialogue(character_meta_1, character_meta_2):
 	role_2_name = role_2['name']
 	role_2_info = role_2['info']
 
-	character_1 = CharacterAgent([],role_1_name,role_1_info,role_2_name,role_2_info)
-	character_2 = CharacterAgent([],role_2_name,role_2_info,role_1_name,role_1_info)
+	character_1 = CharacterAgent([],bot_name=role_1_name,bot_info=role_1_info,user_name=role_2_name,user_info=role_2_info)
+	character_2 = CharacterAgent([],bot_name=role_2_name,bot_info=role_2_info,user_name=role_1_name,user_info=role_1_info)
+
+
+	chat_history = []
+
+	content = "你好"
+
+	for i in range(20):
+		# Character 1 speaks
+		content = character_1.chat(content)
+		# Character 2 responds
+		print(f"{role_1_name}: {content}")
+		chat_history.append(f"{role_1_name}: {content}")
+
+		content = character_2.chat(content)
+		# Check if the dialogue should end
+		print(f"{role_2_name}: {content}")
+		chat_history.append(f"{role_2_name}: {content}")
+
+		# 将对话内容输出显示在屏幕上
+		dialogue_output = "\n".join(chat_history)
+
+		# 将对话内容导出为文本文件
+		with open("dialogue.txt", "w") as file:
+			file.write("\n".join(chat_history))
 		
-	
+	return dialogue_output
 
 # 创建 Gradio 接口
 character_meta_1 = gr.Textbox(lines=5, label="角色1信息")
-
 character_meta_2 = gr.Textbox(lines=5, label="角色2信息")
 
 
 iface = gr.Interface(generate_dialogue, 
-                     [character_meta_1, character_meta_2],
-                     "text", 
-                     title="角色扮演对话生成工具")
+	[character_meta_1, character_meta_2],
+	"text", 
+	title="角色扮演对话生成工具")
+
 iface.launch(share=True)
+
+
---
+++ characterGLM/homework.md
@@ -0,0 +1,44 @@
+# 作业一
+
+改进 characterglm_api_demo_streamlit.py 的代码，为文生图功能加上风格选项，并在页面上加一个可指定图片风格的选项框。
+
+
+![alt text](image-1.png)
+
+# 作业二
+
+实现 role-play 对话数据生成工具，要求包含下列功能：
+
+基于一段文本（自己找一段文本，复制到提示词就可以了，比如你可以从小说中选取一部分文本，注意文本要用 markdown 格式）生成角色人设，可借助 ChatGLM 实现。
+给定两个角色的人设，调用 CharacterGLM 交替生成他们的回复。
+将生成的对话数据保存到文件中。
+（可选）设计图形界面，通过点击图形界面上的按钮执行对话数据生成，并展示对话数据。
+
+完成情况说明：
+1、在api.py中新增了函数，用生成2个角色的角色名和人设。
+```python
+def generate_role_NameandInfo(role_profile: str) -> Generator[str, None, None]:
+    """ 用chatglm根据文本生成角色的名和人设 """
+    
+    instruction = f"""
+请从下列文本中，抽取出所描写对象的角色名；并抽取出其角色人设信息,不超过200字。以下面格式返回。如
+{{
+    "name": "鲁迅",
+    "info": "浙江绍兴人。他是一位著名文学家、思想家、革命家、教育家、民主战士，是新文化运动的重要参与者，也是中国现代文学的奠基人之一"
+}}
+文本：
+{role_profile}
+"""
+    return get_chatglm_response_via_sdk(
+        messages=[
+            {
+                "role": "user",
+                "content": instruction.strip()
+            }
+        ]
+    )
+```
+2、交替进行自动会话，程序里设置参数为20轮。
+![alt text](image.png)
+3、会话保存在characterGLM\dialogue.txt
+4、使用gradio提供了可视化操作页面，点击flag可用保存会话内容。
